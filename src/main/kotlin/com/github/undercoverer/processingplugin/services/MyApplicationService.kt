052fdddb0c55a1e50c28232642bef7a78471b8d1
GitHub Action <action@github.com>
2021-03-08 00:17:17 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/undercoverer/processingplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.undercoverer.processingplugin.services
+
+import com.github.undercoverer.processingplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/undercoverer/processingplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/undercoverer/processingplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.undercoverer.processingplugin.services
 
+import com.github.undercoverer.processingplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
